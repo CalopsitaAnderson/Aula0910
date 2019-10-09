825bbdfe40c3228fa178a83479a6ff17cb9db685
am33zhick <am33zhick@gmail.com>
2019-10-09 20:44:18 -0300

    aula0910

---
+++ app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
@@ -0,0 +1,93 @@
+package br.com.etechoracio.atividade;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ListView;
+        import android.widget.PopupMenu;
+
+public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener, AdapterView.OnItemLongClickListener,
+        PopupMenu.OnMenuItemClickListener {
+
+    private boolean insertMode;
+    private ItemAdapter adapter;
+    private ListView listView;
+
+    private String selectedItemName;
+    private int selectedItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new ItemAdapter(this);
+
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onItem(String name) {
+        if (insertMode) {
+            adapter.insertItem(name);
+        } else {
+            adapter.updateItem(selectedItem, name);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.atividade, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.chamar_customDialog:
+                CustomDialog dialog = new CustomDialog(this);
+                dialog.show(getFragmentManager(), "customDialog");
+                insertMode = true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.inflate(R.menu.popup);
+        popup.setOnMenuItemClickListener(this);
+        popup.show();
+
+        selectedItem=i;
+
+        selectedItemName=adapter.getItem(i).toString();
+
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+
+            case R.id.action_editar:
+                CustomDialog dialog = new CustomDialog(this);
+                dialog.show(getFragmentManager(), "customDialog");
+                dialog.setItem(selectedItemName);
+                insertMode = false;
+                break;
+            default:
+                adapter.removeItem(selectedItem);
+        }
+
+        return true;
+    }
+}
